bd834e8ba128c8d91ebee88b3a93a3459fc6f7d9
nilshao <nielhaochina@gmail.com>
2021-01-10 21:21:14 +0100

    s

--- play.c
+++ play.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <math.h>
+
+struct point {
+    int x;
+    int y;
+};
+struct rectangle {
+    struct point p1;
+    struct point p2;
+};
 
 int main() {
-    int n1,n2;
-    n1 = 19;
-    n2 = 39;
-    printf("%d\n",n1<<4);
-    printf("%d\n",n2>>1);
-  
-   
-    
+    int x1,y1;
+    int x2,y2,x3,y3;
+    scanf("%d%d",&x1,&y1);
+    scanf("%d%d%d%d",&x2,&y2,&x3,&y3);
+    struct point p = {x1,y1};
+    struct rectangle rec = {{x2,y2},{x3,y3}};
+    if((y2-y1)*(y1-y3)>=0 && (x2-x1)*(x1-x3)>=0){
+        printf("YES\n");return 0;
+    }
+    printf("NO\n");
     return 0;
 }
