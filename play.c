9d9f6d989151835436632d8fa172b7d6786f69ce
nilshao <nielhaochina@gmail.com>
2021-01-17 12:26:44 +0100

    s

--- play.c
+++ play.c
@@ -1,62 +1,12 @@
 #include <stdio.h>
 
-int search(int *data, int length, int value) {
-    int i=0;
-    for(i=0; i<length; ++i){
-        if(data[i]==value){
-            printf("%d ",i+1);
-            return i;
-        }
-        else if(data[i]>value){
-            printf("%d ",i+1);
-            return -1;
-        }
-    }
-    printf("%d ",i);
-    return -1;
-}
+
 
 int main() {
-    /*
-    int a[101];
-    for(int i=0;i<=100;++i){
-        a[i]=0;
-    }
-    
-    int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;++i){
-        scanf("%d",a[i]);
-    }
-//    int[n]=int[0]-1;
-    
+    do{
+        printf("HELLO!\n");
 
-    */
-    int a[100];
-    for(int i=0;i<100;++i){
-        a[i]=0;
-    }
-    
-    int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;++i){
-        scanf("%d",&a[i]);
-    }
-  
-    
-    
-    int p;
-    int m;
-    scanf("%d",&m);
-    for(int i=0;i<m;++i){
-        scanf("%d",&p);
-        int res = search(a, n, p);
-        if( res == -1){
-            printf("failed\n");
-        }else{
-            printf("%d\n", res);
-        }
-        
-    }
+    }while(1>2);
+
     return 0;
 }
