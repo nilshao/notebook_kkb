f5083eb95c36c6c6c356357c70faa366e0f41a4c
nilshao <nielhaochina@gmail.com>
2021-01-23 22:47:50 +0100

    S

--- play.c
+++ play.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
+#include <math.h>
 
-
+void unit(int i,int j){
+    printf("%d*%d=%d",i,j,i*j);
+    return ;
+}
 
 int main() {
-    do{
-        printf("HELLO!\n");
+    /*
+    int n;
+    scanf("%d",&n);
+    double res=13;
+    if(n<=3){
+        res = 13;
+    }else{
+        res+=(n-3)*2.3;
+    }
+    printf("%.1lf\n",res+1);
+*/
 
-    }while(1>2);
-
+    char x;
+    int num;
+    while(~scanf("%c",&x)){
+        if(x>='0'&&x<='9'){
+            num = x-'0';
+        }
+        else{
+            if(num%2==0){
+                printf("YES\n");
+            }else{
+                printf("NO\n");
+            }
+            
+        }
+    }
+    
+    
     return 0;
 }
